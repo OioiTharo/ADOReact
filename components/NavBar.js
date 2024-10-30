22caf6cfbb6a17025ebc709c1713d1864176e3e8
OioiTharo <thais.randrade5@senacsp.edu.br>
2024-10-30 20:43:44 -0300

    ok

--- components/NavBar.js
+++ components/NavBar.js
@@ -19,11 +19,10 @@ const Logo = styled.i`
 const Menu = styled.ul`
   display: flex;
   list-style-type: none;
-  text-decoration: none;
   gap: 20px;
 `;
 
-const MenuItem = styled.li`
+const MenuItem = styled.a`
   color: white;
   font-family: "Jost", sans-serif;
   text-decoration: none;
@@ -40,14 +39,16 @@ const NavBarComponent = () => {
     <NavBar>
       <Menu>
         {['/', '/semana', '/adicionar'].map((path, index) => (
-          <Link key={index} href={path} passHref>
-            <MenuItem
-              className={activeMenu === path ? 'active' : ''}
-              onClick={() => updateActiveMenu(path)}
-            >
-              {path === '/' ? 'Dia' : path.charAt(1).toUpperCase() + path.slice(2)}
-            </MenuItem>
-          </Link>
+          <li key={index}>
+            <Link href={path} passHref>
+              <MenuItem
+                className={activeMenu === path ? 'active' : ''}
+                onClick={() => updateActiveMenu(path)}
+              >
+                {path === '/' ? 'Dia' : path.charAt(1).toUpperCase() + path.slice(2)}
+              </MenuItem>
+            </Link>
+          </li>
         ))}
       </Menu>
     </NavBar>
